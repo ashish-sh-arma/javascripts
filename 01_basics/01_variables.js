262f9a3de7f7b4ab8ebc602ec084523f6873bd57
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-03-06 14:36:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const a=123
+let b="ashish"
+var c="sharma"
+name= "ashish1"
+b="sharma"
+c="ashish"
+name ="ash"
+let d;
+//a=234
+console.log(a)
+console.table([a,b,c,name,d])
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+let a="ashish"
+var b=123
+console.log(typeof a)
+console.log(typeof b)
