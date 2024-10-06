85b193a6f23d9b233600752c1b75cdbe20cef908
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-06 20:41:20 +0000

    datatype

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,42 @@
+// primitive
+// non-primitive
+/*
+primitive
+7 types
+string
+number
+boolean
+null
+undefined
+symbol
+BigInt
+*/
+
+/*
+non-primitive(reference type)
+array
+objects
+functions
+
+*/
+
+
+const score=100
+
+
+let a=Symbol("123");
+let b=Symbol("123");
+console.log(a===b);
+
+const hero=["ashish","sharma"];
+let myobd={
+    name:"ashish",
+    age:13,
+}
+
+
+const ash=function(){
+    console.log("ashish");
+    
+}
+console.log(typeof a);
