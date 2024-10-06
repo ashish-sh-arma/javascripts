e3293932e1f436df95be9de000c6a074bf310be8
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-06 21:49:27 +0000

    mathandnum

---
+++ 01_basics/06_numandmaths.js
@@ -0,0 +1,36 @@
+
+
+
+// const a=1234
+// console.log(a);
+// const b=new Number(1234);
+// console.log(b);
+// console.log(b.toString());
+
+
+
+// const bal=123.66
+// console.log(bal.toFixed(4));
+
+// console.log(bal.toPrecision(4));
+
+// const val=12346275372
+// console.log(val.toLocaleString('en-IN'));
+
+
+
+//--------------------maths------------------------------------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+//console.log(Math.sqrt(4));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
