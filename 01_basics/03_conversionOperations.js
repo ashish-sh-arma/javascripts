9cf442a92879ae5090c87738f5e61d067f9498ce
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-06 19:38:03 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+// let score='3abc'
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+
+// basic conversion
+//"33"-> 33
+//"33abc"->NaN
+// true- 1
+// false -0
+
+let loggedin="";
+let boolloggedin= Boolean(loggedin);
+//console.log(boolloggedin)
+
+/*
+1- true
+0- false
+string - true
+empty string- false
+*/
+
+
+let number=33
+let stnum=String (number)
+console.log(typeof stnum);
+
