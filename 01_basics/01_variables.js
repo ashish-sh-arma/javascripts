dc8527659e68d6630438d294736c0961de40610b
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-06 19:24:31 +0000

    datatype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
 const a=123
 let b="ashish"
 var c="sharma"
-name= "ashish1"
+name1= "ashish1"
 b="sharma"
 c="ashish"
-name ="ash"
+name1 ="ash"
 let d;
 //a=234
 console.log(a)
-console.table([a,b,c,name,d])
+console.table([a,b,c,name1,d])
 
 
 
--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,24 @@
 let a="ashish"
-var b=123
+let b=123
 console.log(typeof a)
 console.log(typeof b)
+
+"use strict";
+
+//alert(3+3)  we are using nodejs, not browser
+console.log(3+3);
+console.log("ashish");
+// code readablity should be high
+// number
+// bigint
+// string="" or ''
+// boolean- true/false
+//null-> standalone value (representation of empty)
+// undefined-> not assign value
+// symbol->unique use in making unique components
+
+
+// object
+
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined
