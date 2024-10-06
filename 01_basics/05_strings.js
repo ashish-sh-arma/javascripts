2a5fc4c6c55b091d6b67d075f5c15dfe7dbb1da1
ashish-sh-arma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-06 21:27:26 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+let a=new String("ashish")
+let b=50
+//console.log(a+b+" ashish");
+
+console.log(`my name is ${a} and my count is ${b}`);
+
+// console.log(a[5]);
+// console.log(a.toUpperCase());
+// console.log(a.charAt(3));
+// console.log(a.indexOf('s'));
+
+
+// const str=a.substring(0,4);
+// console.log(str);
+// const str2=a.slice(-5,4);
+// console.log(str2);
+
+// const newstr="    Ashish   "
+// console.log(newstr);
+// console.log(newstr.trim());
+// console.log(newstr);
+
+const str="ashish--sharma"
+console.log(str.replace('sh','_x'));
+
+console.log(str.includes('sg'));
+
+console.log(str.split('sh'));
+
+
