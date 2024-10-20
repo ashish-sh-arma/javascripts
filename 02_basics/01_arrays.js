d70d1d772d1eb5ba6b5a07479e365e9706e88d53
Ashish Sharma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-20 19:38:48 +0000

    change

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,29 @@
+// array
+
+// const myarr=[1,2,3,4,true,"Ashish"];
+// console.log(myarr[5]);
+// const arr=new Array(1,2,3,4);
+// console.log(arr.length);
+// console.log(myarr);
+// arr.unshift(9);
+// console.log(arr);
+// arr.shift();
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(9));
+
+// newarr=myarr.join();
+// console.log(typeof(newarr));
+// console.log(newarr);
+ const arr=[1,2,3,4,5,6];
+ console.log(arr);
+ const arr1=arr.slice(0,3);
+ console.log(arr1);
+ console.log(arr);
+ const arr2=arr.splice(0,3);
+ console.log(arr2);
+ console.log(arr);
+ 
+ 
+ 
+ 
+ 
