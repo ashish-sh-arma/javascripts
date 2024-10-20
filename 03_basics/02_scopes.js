d4d585fb81d6e8580b1de37998036dae524223a9
Ashish Sharma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-20 21:12:58 +0000

    new

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,11 @@
+if(true){
+    let a=10
+    const b=20
+    var c=30
+}
+
+//console.log(a);  give error not defined
+//console.log(b);   give error not defined
+console.log(c); //not give error as this is declared as global
+
+
