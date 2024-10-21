d12d93bffaece037aa86a5a5308b77f2ba91d93b
Ashish Sharma <162458830+ashish-sh-arma@users.noreply.github.com>
2024-10-21 17:34:36 +0000

    change

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,6 +6,56 @@ if(true){
 
 //console.log(a);  give error not defined
 //console.log(b);   give error not defined
-console.log(c); //not give error as this is declared as global
+//console.log(c); //not give error as this is declared as global
+
+
+
+// nested scope
+
+function one(){
+    const username="ashish"
+    function two(){
+        const website ="youtube"
+        //console.log(username);
+        
+    }
+    //console.log(website);
+    two();
+    
+}
+one()
+//two()
+
+
+
+
+if(true){
+    const username="ashish"
+    if(username==="ashish"){
+        const website="youtube"
+        //console.log(username+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+
+
+// intresting --------------------
+
+function addone(num){
+    return num+1;
+}
+addone(5);
+
+
+//addtwo(5)
+const addtwo=function(num){
+    return num+2
+}
+addtwo(5)
+console.log(addtwo(5));
 
 
